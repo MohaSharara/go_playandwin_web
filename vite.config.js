877cdd33cea99d7aa5447a7545f2006c09673bfd
Mohammad Sharara <mohamad.sharara@numbase.com>
2025-03-19 12:17:08 +0200

    build fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import * as process from "node:process";
+import * as process from 'node:process';
 
-export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+export default defineConfig(({ command, mode }) => {
     // Load the correct .env file based on the mode (development, production, local)
-    const env = loadEnv(mode, process.cwd(), '')
-
+    const env = loadEnv(mode, process.cwd(), '');
 
     console.log(`Base URL: ${env.VITE_BASE_URL}`);
     console.log(`Mode is : ${mode}`);
@@ -14,7 +13,7 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
     return {
         plugins: [react()],
         server: {
-            host: "goplayandwin.local.com",
+            host: 'goplayandwin.local.com',
             port: 3000,
             open: true,
             hot: true,
@@ -29,8 +28,14 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
         },
         define: {
             __APP_ENV__: JSON.stringify(env.APP_ENV),
-            "import.meta.env.VITE_BASE_URL": JSON.stringify(env.VITE_BASE_URL),
-            "import.meta.env.VITE_SECRET_KEY": JSON.stringify(env.VITE_SECRET_KEY),
+            'import.meta.env.VITE_BASE_URL': JSON.stringify(env.VITE_BASE_URL),
+            'import.meta.env.VITE_SECRET_KEY': JSON.stringify(env.VITE_SECRET_KEY),
+        },
+        build: {
+            target: 'esnext', // To allow top-level await
+            rollupOptions: {
+                external: ['jquery', 'bootstrap', 'fingerprint2', 'Winwheel', 'TweenMax'], // Avoid bundling these scripts
+            },
         },
     };
 });
