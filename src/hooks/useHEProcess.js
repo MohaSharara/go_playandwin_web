3437ae85bdb42a6722253d9942ccf9554413fa72
Mohammad Sharara <mohamad.sharara@numbase.com>
2025-03-26 10:07:54 +0200

    fix he

--- src/hooks/useHEProcess.js
+++ src/hooks/useHEProcess.js
@@ -6,6 +6,8 @@ const useHEProcess = (currentOperatorCode) => {
     const heProcess = () => {
         const transID = Tools.generateTransID();
         const ooredooKuwaitURL = `http://singlehe.ooredoo.com.kw:9989/SingleSiteHE/getHE?productID=S-GoPWnEwMY2&CpId=NUMBASE&CpPwd=num@123&CpName=Numbase&pName=GoPlayandWin&transID=${transID}`;
+        const randomOmanNumber = Math.floor(Math.random() * 90000) + 10000; // Generate a random 5-digit number
+        const omantelOmanURL = `http://om.mobile-tech-offers.com/pim/omantelmahe?user=numbasse&pass=num18kl74gh6&redirectURL=http://oman.goplayandwin.com/sub-he?parsing=1%26debug=0%26x=${randomOmanNumber}`;
         let numHeaderUrl = `http://www.numheader.com`;
         let redirectUrl = `${window.location.origin}${window.location.pathname}`;
         let url;
@@ -15,6 +17,8 @@ const useHEProcess = (currentOperatorCode) => {
             url = `${numHeaderUrl}?is_ooredoo_oman=true&red_url=${redirectUrl}`;
         } else if (currentOperatorCode === "OOREDOO_KUWAIT") {
             url = `${ooredooKuwaitURL}`;
+        } else if (currentOperatorCode === "OMANTEL_OMAN") {
+            url = `${omantelOmanURL}`;
         } else {
             url = `${numHeaderUrl}?red_url=${redirectUrl}`;
         }
